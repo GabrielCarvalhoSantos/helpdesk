bb7392c573bff6d63d4065e33076f91dd80eda33
Everson Menezes Santos <eversonx100@gmail.com>
2025-05-11 20:26:45 -0300

    create enums

---
+++ helpdesk/src/main/java/integrador2/helpdesk/enums/Status.java
@@ -0,0 +1,10 @@
+package integrador2.helpdesk.enums;
+
+public enum Status {
+    ABERTO,
+    EM_ANALISE,
+    EM_ATENDIMENTO,
+    AGUARDANDO_CLIENTE,
+    RESOLVIDO,
+    FECHADO
+}
---
+++ helpdesk/src/main/java/integrador2/helpdesk/enums/UserType.java
@@ -0,0 +1,7 @@
+package integrador2.helpdesk.enums;
+
+public enum UserType {
+    CLIENTE,
+    TECNICO
+    // ADMINISTRADOR e SUPERVISOR podem ser acrescentados depois, se precisarmos.
+}
